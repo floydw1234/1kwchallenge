126290cca9a0afe283b53e4482b8d954a142e2de
root <root@cpmqtt1.calit2.uci.edu>
2017-06-19 11:33:13 -0700

    changed port to 8080 so it wouldnt conflict with smartmon

--- app.js
+++ app.js
@@ -165,10 +165,10 @@ app.post("/updateLeaderboard",function(req,res){
 
 
 
-
+ 
 module.exports = app;
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('1kwchallenge app listening on port 8080!');
 });
 
 
